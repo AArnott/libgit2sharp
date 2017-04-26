c7c10db247d1302d9b828de0298a51d8e7138f17
Andrew Arnott <andrewarnott@gmail.com>
2017-04-25 23:08:26 -0700

    Build and test net40 on Travis

--- buildandtest.sh
+++ buildandtest.sh
@@ -12,10 +12,22 @@ export LD_LIBRARY_PATH=.
 # Build release for the code generator and the product itself.
 export Configuration=release
 
-# On linux we don't pack because we can't build for net40.
-# We just build for CoreCLR and run tests for it.
+# Get mono MSBuild to work with .NET SDK projects so we can build
+# for both netstandard and net40.
+# https://github.com/dotnet/sdk/issues/335#issuecomment-291487227
+# https://github.com/OmniSharp/omnisharp-roslyn/blob/ef3d302484fc6c86cfa744d25d9149200f090d31/msbuild.sh
+SDK_DIR="/usr/share/dotnet/sdk/1.0.1/"
+export MSBuildExtensionsPath=$SDK_DIR
+export CscToolExe=$SDK_DIR/Roslyn/RunCsc.sh
+export MSBuildSDKsPath=$SDK_DIR/Sdks
+
 dotnet restore
-dotnet build LibGit2Sharp.Tests -f netcoreapp1.0 /property:ExtraDefine="$EXTRADEFINE" /fl /flp:verbosity=detailed
+msbuild /v:minimal /m /property:ExtraDefine="$EXTRADEFINE" /fl /flp:verbosity=detailed /t:build,pack
+
+# Test on CoreCLR
 dotnet test LibGit2Sharp.Tests/LibGit2Sharp.Tests.csproj -f netcoreapp1.0 --no-build
+
+# Test on mono with the net40 assembly
+mono ~/.nuget/packages/xunit.runner.console/2.2.0/tools/xunit.console.exe bin\LibGit2Sharp.Tests\$Configuration\net46\LibGit2Sharp.Tests.dll -noshadow
 
 exit $?
